88cd73854801b752f0072218a274d092e9389f4e
iabkadri <iabkadri@student.1337.ma>
2023-04-16 19:50:27 +0000

    remove unused symbols

--- includes/symbols.h
+++ includes/symbols.h
@@ -6,16 +6,10 @@ typedef enum e_tokentype
 	REDIR,
 	PIPE,
 	WORD,
-	ENVVAR,
-	DQUOTE,
-	SQUOTE,
-	CMD,
-	TREE,
 	REDIR_IN,
 	REDIR_OUT,
 	REDIR_OUT_APPEND,
 	HEREDOC,
-	ARGS,
 	END,
 	NIL
 }	t_tokentype;
